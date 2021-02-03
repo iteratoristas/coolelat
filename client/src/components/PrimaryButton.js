7da7de2b882dd1d79edd8d3ee3dc0459768a4446
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-04 01:01:58 +0800

    feat: create primary component

---
+++ client/src/components/PrimaryButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function PrimaryButton(props) {
+  return (
+    <button className="primary_btn" type={props.type}>
+      <span className="primary_btn_text">{props.text}</span>
+    </button>
+  );
+}
+
+PrimaryButton.defaultProps = {
+  text: "Start Quiz",
+  type: "button",
+};
+
+PrimaryButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["button", "submit"]),
+};
+
+export default PrimaryButton;
