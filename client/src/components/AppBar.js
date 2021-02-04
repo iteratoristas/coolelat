ede50a3cac09f0d5696c929a53ea579ccb3b7799
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-04 16:43:41 +0800

    feat: add preliminary app bar component

---
+++ client/src/components/AppBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavLink from './NavLink';
+
+export default function AppBar() {
+  return (
+    <header className="appbar">
+      <h2 className="appbar_logo">Coolelat</h2>
+      <nav className="appbar_nav">
+        <NavLink href="/" text="Categories"/>
+        <NavLink href="/" text="About"/>
+      </nav>
+    </header>
+  );
+}
