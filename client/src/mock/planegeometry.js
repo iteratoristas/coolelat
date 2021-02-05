0b32fab1a866c57a17d2646fbf629cc57b9f7fdf
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-05 16:04:38 +0800

    feat: add mock data for plane geometry component

---
+++ client/src/mock/planegeometry.js
@@ -0,0 +1,112 @@
+const data = [
+    {
+        "question": "A nine-sided polygon is called:",
+        "answer": "a nonagon",
+        "options": [
+          { "answer": "a hexagon" },
+          { "answer": "a heptagon" },
+          { "answer": "a nonagon" },
+          { "answer": "a decagon" }
+        ],
+        "explanation": "A nine-sided polygon as a nonagon."
+    },
+    {
+        "question": "What do we call a twelve-sided polygon?",
+        "answer": "a decagon",
+        "options": [
+          { "answer": "a decagon" },
+          { "answer": "a hendecagon" },
+          { "answer": "a dodecagon" },
+          { "answer": "an icosagon" }
+        ],
+        "explanation": "A twelve-sided polygon is a dodecagon."
+    },
+    {
+        "question": "How many sides does a heptagon have?",
+        "answer": "7",
+        "options": [
+          { "answer": "6" },
+          { "answer": "7" },
+          { "answer": "8" },
+          { "answer": "9" }
+        ],
+        "explanation": "A heptagon has seven sides."
+    },
+    {
+        "question": "A five-sided polygon is called:",
+        "answer": "a pentagon",
+        "options": [
+          { "answer": "a pentagon" },
+          { "answer": "a hexagon" },
+          { "answer": "a nonagon" },
+          { "answer": "a decagon" }
+        ],
+        "explanation": "A five-sided polygon is a pentagon."
+    },
+    {
+        "question": "How many sides does a dodecagon have?",
+        "answer": "12",
+        "options": [
+          { "answer": "9" },
+          { "answer": "10" },
+          { "answer": "11" },
+          { "answer": "12" }
+        ],
+        "explanation": "A dodecagon has twelve sides."
+    },
+    {
+        "question": "What do we call a six-sided polygon?",
+        "answer": "a hexagon",
+        "options": [
+          { "answer": "a heptagon" },
+          { "answer": "a hencagon" },
+          { "answer": "a hexagon" },
+          { "answer": "an pentagon" }
+        ],
+        "explanation": "A six sided polygon is a hexagon. The name comes from the Greek word hexagnos, which means six angles."
+    },
+    {
+        "question": "A triangle is a closed planar shape with:",
+        "answer": "3 sides",
+        "options": [
+          { "answer": "2 sides" },
+          { "answer": "4 sides" },
+          { "answer": "3 sides" },
+          { "answer": "5 sides" }
+        ],
+        "explanation": "A triangle is a polygon with 3 sides. The name comes from the Latin word tri, which means three."
+    },
+    {
+        "question": "Two parallel lines intersect.",
+        "answer": "False",
+        "options": [
+          { "answer": "True" },
+          { "answer": "False" }
+        ],
+        "explanation": "Two parallel lines do not intersect."
+    },
+    {
+        "question": "Which of the statements below best describes a square?",
+        "answer": "A square has 4 equal sides and 4 right angles.",
+        "options": [
+          { "answer": "A square has 4 equal sides and 4 right angles." },
+          { "answer": "A square has 4 equal sides." },
+          { "answer": "A square has 4 right angles." },
+          { "answer": "A square has 2 pairs of parallel sides." }
+        ],
+        "explanation": "Two parallel lines do not intersect."
+    },
+    {
+        "question": "An equilateral triangle has:",
+        "answer": "all 3 sides equal",
+        "options": [
+          { "answer": "only 2 equal sides" },
+          { "answer": "all 3 sides equal" },
+          { "answer": "no equal sides" },
+          { "answer": "one right angle" }
+        ],
+        "explanation": "An equilateral triangle is a type of triangle where all three sides equal and all three angles equal."
+    }
+]
+
+export default data;
