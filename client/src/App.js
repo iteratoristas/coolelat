56752ebeb6505316b27f6af4756d146a21c5d423
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-05 16:07:53 +0800

    feat: show plane geometry screen in App()

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import PrimaryButton from "./components/PrimaryButton";
 import AppBar from "./components/AppBar";
 import QuizCard from "./components/QuizCard";
+import PlaneGeometry from "./screens/PlaneGeometry";
 
 function App() {
   return (
     <>
       <AppBar />
-      <QuizCard/>
+      {/* <QuizCard/>
       <PrimaryButton text="Login" type="button" />
-      <PrimaryButton type="button" />
+      <PrimaryButton type="button" /> */}
+      <PlaneGeometry/>
     </>
   );
 }
