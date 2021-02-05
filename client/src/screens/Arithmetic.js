c62d2e41b3e499bbc7eb12d1fbaad1a8bbdd4c76
Majella Jereza <majella.jereza.phowzzz@gmail.com>
2021-02-06 00:11:20 +0800

    addition arithmetic questions

---
+++ client/src/screens/Arithmetic.js
@@ -0,0 +1,19 @@
+import React from "react";
+import add from "../mock/arithmetic/addition";
+import Question from "../components/Question";
+
+export default function arithmetic() {
+  return add.map((element, index) => {
+    return (
+      <Question
+        key={index}
+        index={index}
+        question={element.question}
+        answer={element.answer}
+        image={element.image}
+        options={element.options}
+        explanation={element.explanation}
+      />
+    );
+  });
+}
