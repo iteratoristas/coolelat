7967b77243742d319e9ea5b1e66325f4da30f030
Majella Jereza <majella.jereza.phowzzz@gmail.com>
2021-02-08 05:54:23 +0800

    Division of two digits by one digit

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,20 @@ import "./App.css";
 import PrimaryButton from "./components/PrimaryButton";
 import AppBar from "./components/AppBar";
 import QuizCard from "./components/QuizCard";
+import Categories from "./components/Categories";
 import PlaneGeometry from "./screens/PlaneGeometry";
 import Arithmetic from "./screens/Arithmetic";
 
 function App() {
   return (
     <>
-      <AppBar />
-      {/* <QuizCard/>
+      <Categories />
+      {/*<AppBar />
+      <QuizCard/>
       <PrimaryButton text="Login" type="button" />
       <PrimaryButton type="button" /> */}
-      <PlaneGeometry/>
-      {/* <Arithmetic/> */}
+      <PlaneGeometry/> 
+       <Arithmetic/> 
     </>
   );
 }
---
+++ client/src/components/Categories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavLink from './NavLink';
+import "./Categories.css";
+
+export default function Categories() {
+  return (
+    <div className="categories">
+    </div>
+  );
+}
