6fa3abcbab02165407874dcdc8c9c09b2d091371
Majella Jereza <majella.jereza.phowzzz@gmail.com>
2021-02-08 07:55:19 +0800

    Inital Categories Page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import PrimaryButton from "./components/PrimaryButton";
 import AppBar from "./components/AppBar";
 import QuizCard from "./components/QuizCard";
-import Categories from "./components/Categories";
 import PlaneGeometry from "./screens/PlaneGeometry";
 import Arithmetic from "./screens/Arithmetic";
 
@@ -10,13 +9,13 @@ function App() {
   return (
     <>
       
-      <AppBar />
-     {/* <QuizCard/>
+      {/*<AppBar />
+      <QuizCard/>
       <PrimaryButton text="Login" type="button" />
       <PrimaryButton type="button" /> 
       <PlaneGeometry/> 
-       <Arithmetic/> 
-       <Categories />*/}
+       <Arithmetic/> */}
+
     </>
   );
 }
--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,1 +1,19 @@
 import React from "react";
+import "./Categories.css";
+
+export default function Categories () {
+  return (
+    <div className="Page">
+      <div className="Coolelat">Coolelat</div>
+      <button className="Home">Home</button>
+      <button className="Categories">Categories</button>
+      <button className="About">About</button>
+      <div className="Title">Categories</div>
+      <button className="Arithmetic">Arithmetic</button>
+      <button className="WordProblems">Word Problems</button>
+      <button className="Fractions">Fractions</button>
+      <button className="Geometry">Geometry</button>
+      <button className="Data">Data</button>
+    </div>
+  )
+}
