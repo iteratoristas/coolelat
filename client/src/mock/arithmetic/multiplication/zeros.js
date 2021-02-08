79980b9d97d39ee839527d3637db9067d93fcdea
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-08 23:22:45 +0800

    refactor: restucture /mock folder organization

---
+++ client/src/mock/arithmetic/multiplication/zeros.js
@@ -0,0 +1,114 @@
+const zero = [
+    {
+        "question": "426 x 10",
+        "answer": "4260",
+        "options": [
+          { "answer": "426" },
+          { "answer": "4260" },
+          { "answer": "42600" },
+          { "answer": "426000" }
+        ],
+        "explanation": "insert solution here"
+    },
+    {
+      "question": "740 x 100",
+      "answer": "74000",
+      "options": [
+        { "answer": "74" },
+        { "answer": "740" },
+        { "answer": "7400" },
+        { "answer": "74000" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "5469 x 10",
+      "answer": "54690",
+      "options": [
+        { "answer": "5469" },
+        { "answer": "54690" },
+        { "answer": "546900" },
+        { "answer": "5469000" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "8714 x 100",
+      "answer": "871400",
+      "options": [
+        { "answer": "87140" },
+        { "answer": "871400" },
+        { "answer": "8714000" },
+        { "answer": "87140000" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "4826 x 1000",
+      "answer": "4826000",
+      "options": [
+        { "answer": "4826" },
+        { "answer": "48260" },
+        { "answer": "482600" },
+        { "answer": "4826000" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "5844 x 1000",
+      "answer": "5844000",
+      "options": [
+        { "answer": "5844000" },
+        { "answer": "584400" },
+        { "answer": "58440" },
+        { "answer": "5044000" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "6302 x 100",
+      "answer": "630200",
+      "options": [
+        { "answer": "63020" },
+        { "answer": "630200" },
+        { "answer": "6302000" },
+        { "answer": "63030" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "5732 x 10",
+      "answer": "57320",
+      "options": [
+        { "answer": "57330" },
+        { "answer": "573200" },
+        { "answer": "573300" },
+        { "answer": "57320" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "11264 x 1000",
+      "answer": "11264000",
+      "options": [
+        { "answer": "112640" },
+        { "answer": "1126400" },
+        { "answer": "11264000" },
+        { "answer": "112640000" }
+      ],
+      "explanation": "insert solution here"
+    },
+    {
+      "question": "74586 x 10",
+      "answer": "745860",
+      "options": [
+        { "answer": "74586000" },
+        { "answer": "745860" },
+        { "answer": "7458600" },
+        { "answer": "745860000" }
+      ],
+      "explanation": "insert solution here"
+    }  
+]
+
+export default zero;
