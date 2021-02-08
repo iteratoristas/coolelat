4ed11c083bfbf2b35f78bd324684dc5e37bbdd39
Charlyn Siaotong <siaotongcharlyn@gmail.com>
2021-02-08 14:20:11 +0800

    feat: add mock data

---
+++ client/src/mock/time.js
@@ -0,0 +1,116 @@
+const data = [
+    {
+        "question": "Which day is five days after Saturday?",
+        "answer": "Thursday",
+        "options": [
+          { "answer": "Monday" },
+          { "answer": "Friday" },
+          { "answer": "Thursday" },
+          { "answer": "Sunday" }
+        ],
+        "explanation": "Thursday is five days after Saturday?"
+    },
+    {
+        "question": "Which day is four days before Sunday?",
+        "answer": "Wednesday",
+        "options": [
+          { "answer": "Saturday" },
+          { "answer": "Friday" },
+          { "answer": "Wednesday" },
+          { "answer": "Thursday" }
+        ],
+        "explanation": "A twelve-sided polygon is a dodecagon."
+    },
+    {
+        "question": "Which of the following pairs of months have exactly the same number of days?",
+        "answer": "March and May",
+        "options": [
+          { "answer": "February and January" },
+          { "answer": "March and May" },
+          { "answer": "April and March" },
+          { "answer": "December and November" }
+        ],
+        "explanation": "March and May have 31 days"
+    },
+    {
+        "question": "Which of the following pairs of months have exactly the same number of days?",
+        "answer": "January and March",
+        "options": [
+          { "answer": "September and December" },
+          { "answer": "January and March" },
+          { "answer": "April and July" },
+          { "answer": "February and June" }
+        ],
+        "explanation": "January and March have 31 days"
+    },
+    {
+        "question": "What day is seventeen days after Tuesday?",
+        "answer": "Friday",
+        "options": [
+          { "answer": "Thursday" },
+          { "answer": "Saturday" },
+          { "answer": "Friday" },
+          { "answer": "Monday" }
+        ],
+        "explanation": "A dodecagon has twelve sides."
+    },
+    {
+        "question": "Approximately how many days are there in four months?",
+        "answer": "120 days",
+        "options": [
+          { "answer": "120 days" },
+          { "answer": "110 days" },
+          { "answer": "130 days" },
+          { "answer": "100 days" }
+        ],
+        "explanation": "A six sided polygon is a hexagon. The name comes from the Greek word hexagnos, which means six angles."
+    },
+    {
+        "question": "How many weeks and days are there from 25th October to 28th December inclusive?",
+        "answer": "9 weeks and 2 days",
+        "options": [
+          { "answer": "9 weeks and 4 days" },
+          { "answer": "9 weeks and 6 days" },
+          { "answer": "9 weeks and 3 days" },
+          { "answer": "10 weeks and 2 days" }
+        ],
+        "explanation": ""
+    },
+    {
+        "question": "What day is twenty days before Thursday?",
+        "answer": "Friday",
+        "options": [
+            { "answer": "Saturday" },
+            { "answer": "Wednesday" },
+            { "answer": "Monday" },
+            { "answer": "Friday" }
+        ],
+        "explanation": "Two parallel lines do not intersect."
+    },
+    {
+        "question": "Which day is five days after Saturday?",
+        "answer": "Thursday",
+        "options": [
+          { "answer": "Thursday" },
+          { "answer": "Friday" },
+          { "answer": "Monday" },
+          { "answer": "Wednesday" }
+        ],
+        "explanation": "Two parallel lines do not intersect."
+    },
+    {
+        "question": `Sean started his holiday on 23rd June.
+        He finished his holiday on 17th July.
+        How many days was that?`,
+        "answer": "all 3 sides equal",
+        "options": [
+          { "answer": "only 2 equal sides" },
+          { "answer": "all 3 sides equal" },
+          { "answer": "no equal sides" },
+          { "answer": "one right angle" }
+        ],
+        "explanation": "An equilateral triangle is a type of triangle where all three sides equal and all three angles equal."
+    }
+]
+
+export default data;
