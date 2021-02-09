2cad149b843edc6cf4b2502b64557016aa1419af
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-09 14:59:27 +0800

    feat: update hrefs in navlinks

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavLink from './NavLink';
+import NavLink from "./NavLink";
 
 export default function AppBar() {
   return (
@@ -7,8 +7,8 @@ export default function AppBar() {
       <h2 className="appbar_logo">Coolelat</h2>
       <nav className="appbar_nav">
         <NavLink href="/" text="Home"/>
-        <NavLink href="/" text="Categories"/>
-        <NavLink href="/" text="About"/>
+        <NavLink href="/categories" text="Categories"/>
+        <NavLink href="/about" text="About"/>
       </nav>
     </header>
   );
