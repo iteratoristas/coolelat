a0213cd967b200ec157fb1c0dec99d08e38b45ae
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-09 15:26:13 +0800

    feat: add onclick fn

--- client/src/components/QuizCard.js
+++ client/src/components/QuizCard.js
@@ -2,6 +2,9 @@ import React from "react";
 import PrimaryButton from "./PrimaryButton";
 
 export default function QuizCard() {
+  // make dynamic
+  const link = window.location.pathname;
+
   return (
     <div className="quiz_card">
         <div className="quiz_card_img">
@@ -9,12 +12,13 @@ export default function QuizCard() {
         </div>
         <div className="quiz_card_body">
         <div className="quiz_card_header">
-          Multiplication by 1
+          Multiplication by 2
         </div>
           <div className="quiz_description">
             Multiplication by two is composed of ten questions sure to help your child master multiplication.
         </div>
-        <PrimaryButton/>
+        {/* make dynamic */}
+        <PrimaryButton onClick={(e) => window.location = link+ '/multiplication'}/>
       </div>
     </div>
   );
