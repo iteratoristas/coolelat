554b0caadb68c770949fdb012d42903fa00469d2
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-09 15:27:31 +0800

    refactor: change '1' to '2' in h1

--- client/src/screens/QuizPage.js
+++ client/src/screens/QuizPage.js
@@ -5,7 +5,7 @@ import WordProblems from "./WordProblems"
 export default function QuizPage() {
     return (
         <>
-        <center><h1>Quiz Name</h1></center>
+        <center><h1>Multiplication by 2</h1></center>
         <WordProblems />
         <center><PrimaryButton class="quiz-button" text="Submit" type="submit" /></center>
         <div className="space"></div>
