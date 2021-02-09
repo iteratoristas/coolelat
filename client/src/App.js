f5c4b09262d7bdca54610202136a0f778005fa9e
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-09 15:28:26 +0800

    refactor: update route for quizpage component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import AppBar from "./components/AppBar";
 import Category from "./screens/Category";
-import QuizPage from './screens/QuizPage';
+import QuizPage from "./screens/QuizPage";
+import QuizCard from "./components/QuizCard";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={() => <div>Home</div>} />
         <Route exact path="/categories" component={Category} />
-        <Route exact path="/categories/:subcategory" component={QuizPage} />
+        <Route exact path="/categories/:category/:subcategory" component={QuizPage} />
         <Route exact path="/about" component={() => <div>About</div>} />
       </Switch>
     </Router>
