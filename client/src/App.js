d2b702c97bf136a5c22d7740225e86e649428546
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-09 14:56:27 +0800

    feat: add quiz page as a router route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import AppBar from "./components/AppBar";
 import Category from "./screens/Category";
+import QuizPage from './screens/QuizPage';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={() => <div>Home</div>} />
         <Route exact path="/categories" component={Category} />
+        <Route exact path="/categories/:subcategory" component={QuizPage} />
         <Route exact path="/about" component={() => <div>About</div>} />
       </Switch>
     </Router>
