3bdfb47bd896aa354ee4c911fef0e783a5d206f3
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-10 23:21:02 +0800

    feat: add quiz route with id parameter

--- server/index.js
+++ server/index.js
@@ -47,6 +47,21 @@ pool.connect((error, client) => {
     );
   });
 
+  app.get("/quiz/:id", (req, res) => {
+    return client.query(
+      "SELECT * FROM quizzes INNER JOIN questions USING(quiz_id) INNER JOIN question_options USING(question_id) WHERE quiz_id = $1",
+      [req.params.id],
+      (error, result) => {
+        if (error) {
+          return res.status(500).json({ error: "Could not retrieve data." });
+        }
+
+        let results = getResult(result, 'options', ['option', 'id'], 'question_id');
+        return res.status(200).json(results);
+      }
+    );
+  });
+
   app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
   });
