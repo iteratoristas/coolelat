0b97ad3155c053a659e5db5ec6039729b3fd318f
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-13 22:03:42 +0800

    refactor: rewrite component to use custom hook [useFetch]

--- client/src/screens/Category.js
+++ client/src/screens/Category.js
@@ -1,37 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import CategoryButton from "../components/CategoryButton";
 import { useData } from "../DataProvider";
 
 export default function Category() {
-  const [categories, setCategories] = useState([]);
-  const data = useData().categories;
-
-  useEffect(() => {
-    if (data.length > 0) {
-      setCategories(data);
-    } else {
-      fetch("http://localhost:5000/categories")
-        .then((response) => response.json())
-        .then((data) => setCategories(data))
-        .catch((reason) => console.log(reason));
-    }
-  }, [data]);
+  const data = useData();
 
   return (
     <>
       <div className="cat-title">
         <h1>Categories:</h1>
       </div>
-      {categories.map((element, index) => {
-        return (
-          <CategoryButton
-            key={index}
-            text={element.name}
-            link={element.name.toLowerCase().replaceAll(" ", "-")}
-            type="button"
-          />
-        );
-      })}
+      {data.loading && <div>Loading...</div>}
+      {data.error && <div>Error {data.error}</div>}
+      {!data.loading &&
+        !data.error &&
+        data.categories &&
+        data.categories.map((element, index) => {
+          return (
+            <CategoryButton
+              key={index}
+              text={element.name}
+              link={element.name.toLowerCase().replaceAll(" ", "-")}
+              type="button"
+            />
+          );
+        })}
     </>
   );
 }
