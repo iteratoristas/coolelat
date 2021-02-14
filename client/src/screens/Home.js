a9bba4ab4e64a4166cc85e1da036f97dd55742d4
Majella Jereza <majella.jereza.phowzzz@gmail.com>
2021-02-14 20:52:36 +0800

    feat: add home page

---
+++ client/src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./HomeButton";
+import HomeButton from "./HomeButton";
+
+export default function Home (){
+    return (
+        <div className="Home">
+            <div className="tagline">Help your kids unlock their potential</div>
+            <div className="mission">We are committed to providing 
+                quality math and science content for young learners 
+                ages 7 to 19</div>
+            <div className="learn">Learn More</div>
+            <HomeButton/>
+        </div>
+    )
+}
