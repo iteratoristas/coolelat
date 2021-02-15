1a9c8d45329af922576ff21155589f31d5dd65eb
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-15 22:19:31 +0800

    feat: add preliminary routing and display content from fetch

--- client/src/screens/SubcatQuizzes.js
+++ client/src/screens/SubcatQuizzes.js
@@ -1,22 +1,31 @@
 import React from "react";
-import QuizCardTop from "../components/QuizCardTop"
-import QuizCard from "../components/QuizCard"
+import QuizCardTop from "../components/QuizCardTop";
+import QuizCard from "../components/QuizCard";
+import { useData } from "../context/DataProvider";
+import useFetch from "../hooks/useFetch";
 
 export default function SubcatQuizzes() {
-    return (
-        <>
-            <div className="cat-title"><h1>Subcategory name</h1></div>
-            <div className = "quiz-box">
-                <QuizCardTop />
-                <QuizCardTop/>
-                <QuizCardTop/>
-            </div>
-            <div>
-                <QuizCard />
-                <QuizCard/>
-                <QuizCard/>
-                <QuizCard/>
-            </div>
-        </>
-    )
+  const data = useData();
+  const paths = window.location.pathname.split("/");
+  const subcategory = paths[paths.length - 1]
+    .split("-")
+    .map((value) => value[0].toUpperCase() + value.substring(1))
+    .join("-");
+  const quiz = data.quizzes.filter(
+    (value) => value.subcategory === subcategory
+  );
+
+  return (
+    <>
+      <div className="cat-title">
+        <h1>{subcategory} Problems</h1>
+      </div>
+      <div className="quiz-box">
+        {quiz.slice(0, 3).map((value) =>  <QuizCardTop quiz={value} />)}
+      </div>
+      <div>
+        {quiz.map((value) => <QuizCard quiz={value} />)}
+      </div>
+    </>
+  );
 }
