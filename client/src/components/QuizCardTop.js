bf422bcdc3249d305dc8d91c32f0fe8c634c6b8f
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-15 21:56:29 +0800

    feat: add quiz prop to quiz card top

--- client/src/components/QuizCardTop.js
+++ client/src/components/QuizCardTop.js
@@ -1,20 +1,20 @@
 import React from "react"
 import PrimaryButton from "./PrimaryButton"
 
-function QuizCardTop() {
+function QuizCardTop({quiz}) {
   const link = window.location.pathname;
 
     return (
         <div className="quizcard-square">
             <div className="qc-title">
-                Multiplication by two
+                {quiz?.quiz_name ?? "Multiplication by two" }
             </div>
             <div className="qc-description">
                 <p>
-                Multiplication by two is composed of ten questions sure to help your child master multiplication.
+                {quiz?.description ?? "Multiplication by two is composed of ten questions sure to help your child master multiplication."}
                 </p>
             </div>
-            <center><PrimaryButton onClick={(e) => window.location = link+ '/multiplication'}/></center>
+            <center><PrimaryButton onClick={(e) => window.location = link+ `quiz:${quiz.quiz_id}`}/></center>
         </div>
     )
 }
