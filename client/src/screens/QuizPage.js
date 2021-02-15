e4b7497c3a86842f601f6b80c2f0ee767de21c21
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-15 23:24:57 +0800

    feat: add correct data and shuffling functionality to quiz page

--- client/src/screens/QuizPage.js
+++ client/src/screens/QuizPage.js
@@ -1,14 +1,53 @@
-import React from "react"
-import PrimaryButton from "../components/PrimaryButton"
-import WordProblems from "./WordProblems"
+import React from "react";
+import PrimaryButton from "../components/PrimaryButton";
+import { useData } from "../context/DataProvider";
+import Question from "../components/Question";
+import randomizer from "../randomizer";
 
-export default function QuizPage() {
-    return (
-        <>
-        <center><h1>Multiplication by 2</h1></center>
-        <WordProblems />
-        <center><PrimaryButton class="quiz-button" text="Submit" type="submit" /></center>
-        <div className="space"></div>
-        </>
-    )
+export default function QuizPage(props) {
+  const data = useData();
+  const quizName = props.match.params.quiz.split("-").map((value) => value.toLowerCase()).join(" ");
+  const quizzes = data.quizzes.filter(value => value.quiz_name.toLowerCase() === quizName);
+
+  return (
+    <>
+      {data.error && <div>Error</div>}
+      {data.loading && <div>Loading...</div>}
+      {!data.error && !data.loading && quizzes && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "space-between",
+            height: "100%",
+            padding: "2rem 0rem",
+            width: "100%",
+          }}
+        >
+          <h1>{quizzes[0].quiz_name}</h1>
+          <div>
+            {randomizer(quizzes).map((element, index) => {
+              return (
+                <Question
+                  key={index}
+                  index={index}
+                  question={element.question}
+                  answer={element.answer}
+                  image={element.image}
+                  options={element.options}
+                  explanation={element.explanation}
+                />
+              );
+            })}
+          </div>
+          <PrimaryButton
+            //   className="quiz-button"
+            text="Submit"
+            type="submit"
+          />
+        </div>
+      )}
+    </>
+  );
 }
