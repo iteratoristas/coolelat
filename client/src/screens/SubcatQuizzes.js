f0406244c9eadba3db103287044bdbf50a678045
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-15 22:23:34 +0800

    feat: add key prop to rendered items

--- client/src/screens/SubcatQuizzes.js
+++ client/src/screens/SubcatQuizzes.js
@@ -1,31 +1,23 @@
 import React from "react";
-import QuizCardTop from "../components/QuizCardTop";
-import QuizCard from "../components/QuizCard";
+import QuizCardTop from "../components/QuizCardTop"
+import QuizCard from "../components/QuizCard"
 import { useData } from "../context/DataProvider";
-import useFetch from "../hooks/useFetch";
 
 export default function SubcatQuizzes() {
-  const data = useData();
-  const paths = window.location.pathname.split("/");
-  const subcategory = paths[paths.length - 1]
-    .split("-")
-    .map((value) => value[0].toUpperCase() + value.substring(1))
-    .join("-");
-  const quiz = data.quizzes.filter(
-    (value) => value.subcategory === subcategory
-  );
+    const data = useData();
+    const paths = window.location.pathname.split('/');
+    const subcategory = paths[paths.length - 1].split('-').map(value => value[0].toUpperCase() + value.substring(1)).join('-')
+    const quiz = data.quizzes.filter((value) => value.subcategory === subcategory);
 
-  return (
-    <>
-      <div className="cat-title">
-        <h1>{subcategory} Problems</h1>
-      </div>
-      <div className="quiz-box">
-        {quiz.slice(0, 3).map((value) =>  <QuizCardTop quiz={value} />)}
-      </div>
-      <div>
-        {quiz.map((value) => <QuizCard quiz={value} />)}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="cat-title"><h1>{subcategory} Problems</h1></div>
+            <div className = "quiz-box">
+                { quiz.slice(0, 3).map((value, index) =>  <QuizCardTop key={index} quiz={value} />) }
+            </div>
+            <div>
+            { quiz.map((value, index) => <QuizCard key={index} quiz={value} />) }
+            </div>
+        </>
+    )
 }
