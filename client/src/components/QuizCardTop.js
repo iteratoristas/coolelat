d7146574e60bcd3ee30ff2b86be42a5d8c14382a
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-15 22:27:07 +0800

    refactor: update onclick location

--- client/src/components/QuizCardTop.js
+++ client/src/components/QuizCardTop.js
@@ -2,7 +2,10 @@ import React from "react"
 import PrimaryButton from "./PrimaryButton"
 
 function QuizCardTop({quiz}) {
+    // useHistory
   const link = window.location.pathname;
+//   extract into helper fn
+  const direction = quiz?.quiz_name.split(' ').map(value => value.toLowerCase()).join('-');
 
     return (
         <div className="quizcard-square">
@@ -14,7 +17,7 @@ function QuizCardTop({quiz}) {
                 {quiz?.description ?? "Multiplication by two is composed of ten questions sure to help your child master multiplication."}
                 </p>
             </div>
-            <center><PrimaryButton onClick={(e) => window.location = link+ `/quiz/${quiz.quiz_id}`}/></center>
+            <center><PrimaryButton onClick={(e) => window.location = link+ `/${direction}`}/></center>
         </div>
     )
 }
