95dfbb917df1b960ea8a2ac4a58dde86d97bdfb4
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-16 12:55:01 +0800

    feat: utilize flow HOC and update render logic based on server response change

--- client/src/screens/SubcatQuizzes.js
+++ client/src/screens/SubcatQuizzes.js
@@ -1,23 +1,45 @@
 import React from "react";
-import QuizCardTop from "../components/QuizCardTop"
-import QuizCard from "../components/QuizCard"
+import QuizCardTop from "../components/QuizCardTop";
+import QuizCard from "../components/QuizCard";
 import { useData } from "../context/DataProvider";
+import { unparsePath } from "../helpers/pathFunctions";
+import Flow from "../components/Flow";
 
-export default function SubcatQuizzes() {
-    const data = useData();
-    const paths = window.location.pathname.split('/');
-    const subcategory = paths[paths.length - 1].split('-').map(value => value[0].toUpperCase() + value.substring(1)).join('-')
-    const quiz = data.quizzes.filter((value) => value.subcategory === subcategory);
+export default function SubcatQuizzes(props) {
+  const data = useData();
+  const subcategory = unparsePath(props.match.params.subcategory);
+  const quiz = data.quizzes
+    .filter((value) => value.subcategory === subcategory)
+    .filter(
+      (value, index, array) => array[index + 1]?.quiz_name !== value?.quiz_name
+    );
 
-    return (
+  return (
+    <Flow
+      loading={data.loading}
+      error={data.error}
+      props={props}
+      categories={data.categories}
+      subcategories={data.subcategories}
+      component={
         <>
-            <div className="cat-title"><h1>{subcategory} Problems</h1></div>
-            <div className = "quiz-box">
-                { quiz.slice(0, 3).map((value, index) =>  <QuizCardTop key={index} quiz={value} />) }
-            </div>
-            <div>
-            { quiz.map((value, index) => <QuizCard key={index} quiz={value} />) }
-            </div>
+          <div className="cat-title">
+            <h1>{subcategory} Problems</h1>
+          </div>
+          <h2>Most Popular</h2>
+          <div className="quiz-box">
+            {quiz.slice(0, 3).map((value, index) => (
+              <QuizCardTop key={index} quiz={value} />
+            ))}
+          </div>
+          <h2>All Problems</h2>
+          <div>
+            {quiz.map((value, index) => (
+              <QuizCard key={index} quiz={value} />
+            ))}
+          </div>
         </>
-    )
+      }
+    />
+  );
 }
