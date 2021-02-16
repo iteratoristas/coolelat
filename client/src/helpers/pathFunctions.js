9a8e5de8b7399dabe45e15f308775d8056ae6520
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-16 11:56:38 +0800

    feat: add path helper functions

---
+++ client/src/helpers/pathFunctions.js
@@ -0,0 +1,27 @@
+import { useData } from "../context/DataProvider";
+
+export const parsePath = (path) => path.trim().split(' ').map(fragment => fragment.toLowerCase()).join('-'); 
+
+export const unparsePath = (path) => path.trim().split('-').map(fragment => fragment[0].toUpperCase() + fragment.substring(1)).join(' ');  
+
+export function validatePath(props, {categories, subcategories}) {
+    const params = props.match.params;
+    const keys = Object.keys(params);
+
+    const boolean = keys.map(value => {
+        switch (value) {
+            case 'category':
+                console.log(unparsePath(params[value]))
+                return categories.map(value => value.name).includes(unparsePath(params[value]))
+            case 'subcategory':
+                return subcategories.map(value => value.subcategory).includes(unparsePath(params[value]))
+            case 'quiz':
+                const quizzes = [...subcategories.map(value => value.quizzes).map(quiz => quiz.quiz_name)];
+                return quizzes.includes(unparsePath(params[value]))
+            default:
+                return false;
+        }
+    });
+
+    return boolean.every(value => value);
+}
