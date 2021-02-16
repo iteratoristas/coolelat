09da10ecb24025c02e81e376a47cf33411bc8990
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-16 12:53:06 +0800

    feat: add props to flow HOC

--- client/src/screens/Subcategory.js
+++ client/src/screens/Subcategory.js
@@ -1,36 +1,30 @@
 import React from "react";
 import CategoryButton from "../components/CategoryButton";
+import Flow from "../components/Flow";
 import { useData } from "../context/DataProvider";
+import { parsePath, unparsePath } from "../helpers/pathFunctions";
 
-export default function Subcategory() {
+export default function Subcategory(props) {
   const data = useData();
 
-  const path = window.location.pathname.split('/');
-  const location = path[path.length - 1].split('-').map(value => value[0].toUpperCase() + value.substring(1)).join(' ');
+  const location = unparsePath(props.match.params.category)
   const category = data.categories.filter(value => value.name === location)[0];
-  const subcategories = category?.subcategories;
 
-  return (
+  return <Flow loading={data.loading} error={data.error} props={props} categories={data.categories} component={
     <>
       <div className="cat-title">
         <h1>{location}</h1>
         <p>{category?.description || "Short description abt the category"}</p>
       </div>
       <div className="category-names">  
-      {!data.loading &&
-        !data.error &&
-        subcategories &&
-        subcategories.map((el, i) => (
+      {category?.subcategories.map((el, i) => (
           <CategoryButton
             key={i}
             text={el.subcategory}
-            link={el.subcategory.toLowerCase()}
+            link={parsePath(el.subcategory)}
           />
         ))}
-      </div>
-      {data.loading && <div>Loading...</div>}
-      {data.error && <div>Error {data.error}</div>}
-      
+      </div>      
     </>
-  );
+  }/>
 }
