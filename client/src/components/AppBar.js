648981b00bfcf5903ad768c4491393fb9036ceca
Charlyn Siaotong <siaotongcharlyn@gmail.com>
2021-02-19 03:59:57 +0800

    fix: fixed the responsive Appbar

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,34 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import NavLink from "./NavLink";
 
-export default function AppBar() {
+class AppBar extends Component {
+  state = { clicked: false }
   
-  const navLinks = document.querySelector("nav-links")
-
-  function showNav() {
-    navLinks.classList.toggle("open")
+  handleClick = () => {
+    this.setState({clicked:!this.state.clicked})
   }
 
-  // hamburger.addEventListener("click", () => {
-  //   navLinks.classList.toggle("open")
-  // })
+  render() {
+    return (
+      <>
+        <nav className="appbar">
+          <h2 className="appbar_logo">Coolelat</h2>
+          <div className="menu-bar" onClick={this.handleClick}>
+            <i className={this.state.clicked ? 'fa fa-times' : 'fa fa-bars'}></i>
+          </div>
 
-  return (
-    <>
-    <nav className="appbar">
-       <h2 className="appbar_logo">Coolelat</h2>
-       <div className="hamburger" onClick={showNav}>
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
-        </div>
-
-        <ul className="nav-links">
-          <li><NavLink href="/" text="Home" /></li>
-          <li><NavLink href="/categories" text="Categories"/></li>
-          <li><NavLink href="/about" text="About"/></li>
-        </ul>
-      </nav>
+          <ul className={this.state.clicked ? 'nav-links active' : 'nav-links'}>
+            <li><NavLink href="/" text="Home" /></li>
+            <li><NavLink href="/categories" text="Categories" /></li>
+            <li><NavLink href="/about" text="About" /></li>
+          </ul>
+        </nav>
       </>
-  );
+    )
+  }
 }
+
+export default AppBar
