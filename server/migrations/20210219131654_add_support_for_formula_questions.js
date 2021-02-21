f519f7d3c7daf5ec2a0b9fdb11bbbf31fc95a7d9
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-21 14:32:11 +0800

    revert: remove formula column in questions table
    use answer column to store formulas instead

--- server/migrations/20210219131654_add_support_for_formula_questions.js
+++ server/migrations/20210219131654_add_support_for_formula_questions.js
@@ -1,14 +1,12 @@
 
 exports.up = function(knex) {
     return knex.schema.table("questions", function (table) {
-        table.text("formula");
         table.enu('type', ['MC', 'FQ']).defaultTo('MC').notNullable();
     });    
 };
 
 exports.down = function (knex) {
     return knex.schema.table("questions", function (table) {
-        table.dropColumn('formula');
         table.dropColumn('type');
     });
 };
