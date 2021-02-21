1bc14eee0dea82698ef4feb8caca45deaa56d5dc
Majella Jereza <majella.jereza.phowzzz@gmail.com>
2021-02-21 16:42:08 +0800

    style: about page design

--- client/src/screens/About.js
+++ client/src/screens/About.js
@@ -2,6 +2,18 @@ import React from "react";
 
 export default function About(){
     return (
-        <div> About us</div>
+        <div className="about-page"> 
+            <div className="about-header">About Us</div>
+            <div className="about-desc">We are committed to providing 
+                quality math and science content for young learners 
+                ages 7 to 19. </div>
+            <div className="about-desc2">"Study Math for a better Path" 
+                is our goal in helping your kids unlock their 
+                potential. Children can learn step-by-step
+                through easy up to hard categories in this 
+                platform.</div>
+            <div className="about-footer"></div>
+           
+        </div>
     )
 }
