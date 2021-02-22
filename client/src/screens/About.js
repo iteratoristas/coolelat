962d3a6bfe95445ec11a8ec66702b472c60700db
Majella Jereza <majella.jereza.phowzzz@gmail.com>
2021-02-22 13:38:50 +0800

    style: about page css

--- client/src/screens/About.js
+++ client/src/screens/About.js
@@ -6,13 +6,11 @@ export default function About(){
             <div className="about-header">About Us</div>
             <div className="about-desc">We are committed to providing 
                 quality math and science content for young learners 
-                ages 7 to 19. </div>
-            <div className="about-desc2">"Study Math for a better Path" 
+                ages 7 to 19. "Study Math for a better Path" 
                 is our goal in helping your kids unlock their 
                 potential. Children can learn step-by-step
                 through easy up to hard categories in this 
                 platform.</div>
-            <div className="about-footer"></div>
            
         </div>
     )
