833b7cd02213f78158c81a453f2204cbf3d39a17
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-02-27 10:21:00 +0800

    feat: add helper for invalid type

--- server/helper/validation.js
+++ server/helper/validation.js
@@ -2,8 +2,10 @@ const isUndefined = (data) => typeof(data) === 'undefined' || data === null;
 const isEmpty = (data) => data.trim() === '';
 const isInvalid = (data) => isUndefined(data) || isEmpty(data);
 const isInvalidPassword = (password) => isInvalid(password) || password.trim().length < 8 || password.trim().length > 16
+const isInvalidType = (type) => isInvalid(type) || !['student', 'teacher'].includes(type)
 
 module.exports = {
     isInvalid,
-    isInvalidPassword
+    isInvalidPassword,
+    isInvalidType
 }
