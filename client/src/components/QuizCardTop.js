7cacd79f10e971607e637407aa1eb3029d7f7317
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-01 00:59:19 +0800

    refactor: utilize navigate()

--- client/src/components/QuizCardTop.js
+++ client/src/components/QuizCardTop.js
@@ -1,25 +1,33 @@
-import React from "react"
-import PrimaryButton from "./PrimaryButton"
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { navigate } from "../helpers/navigate";
+import { parsePath } from "../helpers/pathFunctions";
+import PrimaryButton from "./PrimaryButton";
 
-function QuizCardTop({quiz, text, path}) {
-    // useHistory
-  const link = window.location.pathname;
-//   extract into helper fn
-  const direction = quiz?.quiz_name.split(' ').map(value => value.toLowerCase()).join('-');
+function QuizCardTop({ quiz, text }) {
+  const history = useHistory();
 
-    return (
-        <div className="quizcard-square">
-            <div className="qc-title">
-                {quiz?.quiz_name ?? "Multiplication by two" }
-            </div>
-            <div className="qc-description">
-                <p>
-                {quiz?.description ?? "Multiplication by two is composed of ten questions sure to help your child master multiplication."}
-                </p>
-            </div>
-            <center><PrimaryButton text={text} onClick={(e) => window.location = link+ `/${direction}`}/></center>
-        </div>
-    )
+  return (
+    <div className="quizcard-square">
+      <div className="qc-title">
+        {quiz?.quiz_name ?? "Multiplication by two"}
+      </div>
+      <div className="qc-description">
+        <p>
+          {quiz?.description ??
+            "Multiplication by two is composed of ten questions sure to help your child master multiplication."}
+        </p>
+      </div>
+      <center>
+        <PrimaryButton
+          text={text}
+          onClick={(e) =>
+            navigate(history, { route: parsePath(quiz?.quiz_name) })
+          }
+        />
+      </center>
+    </div>
+  );
 }
 
 export default QuizCardTop;
