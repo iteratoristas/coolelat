eeb5d3926c7a99a8df1ce67e9fa7387c97f5eb15
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-01 00:10:03 +0800

    refactor: add null safety

--- client/src/helpers/navigate.js
+++ client/src/helpers/navigate.js
@@ -1,7 +1,7 @@
 export const navigate = (history, route, { method } = { method: "PUSH" }) => {
   if (method.trim().toUpperCase() === "PUSH") {
-    return history.push(route);
+    return history?.push(route);
   } else if (method.trim().toUpperCase() === "REPLACE") {
-    return history.replace(route);
+    return history?.replace(route);
   }
 };
