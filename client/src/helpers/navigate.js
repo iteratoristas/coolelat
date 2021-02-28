f5f8141aeb2bdea976c13e546671c802518a2ac4
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-01 00:50:57 +0800

    fix: fix null safety

--- client/src/helpers/navigate.js
+++ client/src/helpers/navigate.js
@@ -1,8 +1,8 @@
 export const navigate = (history, { method = "PUSH", route = ""  } = {}) => {
   if (method.trim().toUpperCase() === "PUSH") {
-    return history?.push(route);
+    return history?.push?.(route);
   } else if (method.trim().toUpperCase() === "REPLACE") {
-    return history?.replace(route);
+    return history?.replace?.(route);
   }
 
   return history;
