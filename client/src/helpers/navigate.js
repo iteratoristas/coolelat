c349f6016a18c1317af89794bc41efe1d97fc731
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-01 01:37:57 +0800

    feat: add formatRoute() helper to navigate

--- client/src/helpers/navigate.js
+++ client/src/helpers/navigate.js
@@ -1,9 +1,17 @@
 export const navigate = (history, { method = "PUSH", route = ""  } = {}) => {
   if (method.trim().toUpperCase() === "PUSH") {
-    return history?.push?.(route);
+    return history?.push?.(formatRoute(route));
   } else if (method.trim().toUpperCase() === "REPLACE") {
-    return history?.replace?.(route);
+    return history?.replace?.(formatRoute(route));
   }
 
   return history;
 };
+
+const formatRoute = (route) => {
+  if (route) {
+    return route.trim().split('/').filter(item => item && item?.trim().length > 0).join('/');
+  }
+
+  return route;
+}
