d8034ab44d143d1d816d0a2c5bc56b21810e6a09
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-01 00:45:52 +0800

    refactor: return history object on invalid method

--- client/src/helpers/navigate.js
+++ client/src/helpers/navigate.js
@@ -1,8 +1,9 @@
 export const navigate = (history, { method = "PUSH", route = ""  } = {}) => {
-    console.log(method)
   if (method.trim().toUpperCase() === "PUSH") {
     return history?.push(route);
   } else if (method.trim().toUpperCase() === "REPLACE") {
     return history?.replace(route);
   }
+
+  return history;
 };
