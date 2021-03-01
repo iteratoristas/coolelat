fb21041669310912be3cbbb8929819ef0eb46ac2
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-01 14:00:42 +0800

    chore: push version of app.js with protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,37 +5,56 @@ import About from "./screens/About";
 import Category from "./screens/Category";
 import Subcategory from "./screens/Subcategory";
 import QuizPage from "./screens/QuizPage";
-import SubcatQuizzes from "./screens/SubcatQuizzes"
+import SubcatQuizzes from "./screens/SubcatQuizzes";
 import DataProvider from "./context/DataProvider";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TeacherHome from "./screens/teacher/TeacherHome";
+import Login from "./screens/auth/Login";
+import AuthProvider from "./context/AuthProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Signup from "./screens/auth/Signup";
+import UnprotectedRoute from "./components/UnprotectedRoute";
+import CreateAQuiz from "./screens/teacher/TeacherCreateQuiz";
+import TeacherEditQuiz from "./screens/teacher/TeacherEditQuiz";
 //import AddAQuestion from "./screens/AddAQuestion";
 //import ScorePage from "./screens/ScorePage";
 
 function App() {
   return (
-    <DataProvider>
-      <Router> 
-        <AppBar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/categories" component={Category} />
-          {/*<Route exact path="/question" component={AddAQuestion} /> */}
-          <Route exact path="/categories/:category" component={Subcategory} />
-          <Route
-            exact
-            path="/categories/:category/:subcategory"
-            component={SubcatQuizzes}
-          />
-          <Route
-            exact
-            path="/categories/:category/:subcategory/:quiz"
-            component={QuizPage}
-          />
-          <Route exact path="/about" component={About} />
-          {/*<ScorePage /> */}
-        </Switch>
-      </Router>
-    </DataProvider>
+    <AuthProvider>
+      <DataProvider>
+        <Router>
+          <AppBar />
+          <Switch>
+            <UnprotectedRoute exact path="/signup" component={Signup} />
+            <UnprotectedRoute exact path="/login" component={Login} />
+            <ProtectedRoute exact path="/" component={TeacherHome} />
+            <ProtectedRoute exact path="/create" component={CreateAQuiz} />
+            <ProtectedRoute exact path="/edit/:quizId" component={TeacherEditQuiz} />
+            {/* <Route exact path="/" component={Home} /> */}
+            <ProtectedRoute exact path="/categories" component={Category} />
+            {/*<Route exact path="/question" component={AddAQuestion} /> */}
+            <ProtectedRoute
+              exact
+              path="/categories/:category"
+              component={Subcategory}
+            />
+            <ProtectedRoute
+              exact
+              path="/categories/:category/:subcategory"
+              component={SubcatQuizzes}
+            />
+            <ProtectedRoute
+              exact
+              path="/categories/:category/:subcategory/:quiz"
+              component={QuizPage}
+            />
+            <Route exact path="/about" component={About} />
+            {/*<ScorePage /> */}
+          </Switch>
+        </Router>
+      </DataProvider>
+    </AuthProvider>
   );
 }
 
